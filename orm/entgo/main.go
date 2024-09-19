83119396aff2de8654473fc15f4e7ec9b211148a
β-Gulf <asd375zxcvb@gmail.com>
2024-09-19 21:00:40 +0800

    first use entgo

---
+++ orm/entgo/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"entgo/ent"
+	"entgo/ent/user"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+)
+
+func main() {
+	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer client.Close()
+	ctx := context.Background()
+	_, err = QueryUser(ctx, client)
+	if err != nil {
+		log.Fatalf("failed querying user: %v", err)
+	}
+}
+
+func CreateUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	u, err := client.User.
+		Create().
+		SetAge(30).
+		SetName("a8m").
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating user: %w", err)
+	}
+	log.Println("user was created: ", u)
+	return u, nil
+}
+
+func QueryUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	u, err := client.User.
+		Query().
+		Where(user.Name("a8m")).
+		// `Only` fails if no user found,
+		// or more than 1 user returned.
+		Only(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed querying user: %w", err)
+	}
+	log.Println("user returned: ", u)
+	return u, nil
+}
